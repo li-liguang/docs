1b64245d280afe8afda79790ae1c499f9dfd6330
李 利广 <liguang.li@emdoor.com>
2024-11-05 15:44:21 +0800

    feat

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,7 +1,6 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
-import useBaseUrl from "@docusaurus/useBaseUrl";
 
 const config: Config = {
   title: "文档",
